aef25a5e6abe4830ddda14f01d6c09ff3c57f298
Nicolas Barrera <nbarrera@gmail.com>
2022-12-06 01:26:22 -0300

    validacion de input hecha en api.yml
    validacion de que no se puede cambiar modificar el id de un course
    
    hecha en el updateCourse
    
    se le hace un delete course.id por las dudas

--- courses/routes.js
+++ courses/routes.js
@@ -18,7 +18,6 @@ router.get("/",
 )
 
 router.get("/:id", passport.authenticate('bearer', { session: false }), (req, res) => {
-  // TODO: Validar el input
   const course = getCourse(req.params.id);
 
   if (course) {
@@ -30,21 +29,17 @@ router.get("/:id", passport.authenticate('bearer', { session: false }), (req, re
 
 router.post("/", passport.authenticate('bearer', { session: false }), (req, res) => {
   const course = req.body;
-  // TODO: Validar el input
   const savedCourse = addCourse(course);
 
   res.json(savedCourse);
 });
 
 router.put("/:id", passport.authenticate('bearer', { session: false }), (req, res) => {
-  // TODO: Validar el input
-  // TODO: validar no se puede cambiar el id del curso
   const course = updateCourse(req.params.id, req.body);
   res.json(course);
 });
 
 router.delete("/:id", passport.authenticate('bearer', { session: false }), (req, res) => {
-  // TODO: Validar el input
   removeCourse(req.params.id);
   return res.json({ status: "ok" });
 });
