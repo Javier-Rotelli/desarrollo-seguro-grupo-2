764bd437936b2789e621b670c4576573f976234d
Nicolas Barrera <nbarrera@gmail.com>
2022-12-06 01:16:26 -0300

    quito un warning de mongoose
    agrego por completitud config en .env
    para mongo host y port
    al final no los use
    igual van con defualt values

--- dbUtil.js
+++ dbUtil.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+// supressing mongo warning
+mongoose.set("strictQuery", false);
+
 let dbConnection = undefined;
 
 export const getDB = async () => {
@@ -8,10 +11,12 @@ export const getDB = async () => {
   }
   const username = process.env.MONGO_USERNAME;
   const password = process.env.MONGO_PASSWORD;
+  const host = process.env.MONGO_HOSTNAME || "127.0.0.1";
+  const port = process.env.MONGO_PORT || 27017;
   const db = process.env.MONGO_DB;
 
   dbConnection = await mongoose.connect(
-    `mongodb://${username}:${password}@127.0.0.1:27017/${db}`
+    `mongodb://${username}:${password}@${host}:${port}/${db}`
   );
   return dbConnection;
 };
