e77f0ece6788e66f01e94585746af636d644b53c
Javier Rotelli <javier.rotelli@gmail.com>
2022-12-06 01:07:45 -0300

    courses in mongo

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -25,19 +25,19 @@ db.createCollection("courses");
 
 db.courses.insertMany([
   {
-    _id: "de47e087-4736-4733-ad67-12f71371636e",
+    _id: "de47e08747364733ad6712f7",
     name: "Diplomatura Desarrollo Seguro de App - 2da. edición",
     initDate: "2022-08-20T03:00:00.000Z",
     enrolled: 25,
   },
   {
-    _id: "4708ab93-854c-49ef-ae38-151dfc5196ff",
+    _id: "4708ab93854c49efae38151d",
     name: "Diplomatura Desarrollo Seguro de App - 1da. edición",
     initDate: "2022-03-05T03:00:00.000Z",
     enrolled: 50,
   },
   {
-    _id: "e3ade20f-5dad-4573-9fa1-9b92cbb7b0d5",
+    _id: "e3ade20f5dad45739fa19b92",
     name: "Diplomatura Desarrollo Seguro de App - 3ra. edición",
     initDate: "2023-03-05T03:00:00.000Z",
     enrolled: 0,
