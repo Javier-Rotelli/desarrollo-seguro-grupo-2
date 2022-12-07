95193d8a869513c7cb0deebe76d74ba2792614e0
Nicolas Barrera <nbarrera@gmail.com>
2022-12-07 16:32:32 -0300

    no dead code

--- users/db.js
+++ users/db.js
@@ -18,11 +18,6 @@ export const getUser = async (username) => {
 };
 
 // This is still unused... :/ maybe we could delete it
-//export const createUser = ({ password, ...rest } = {}) => {
-//  const newUser = { ...rest, password: hash(password) };
-//  users.push(newUser);
-//  writeFileSync(USERS_FILE, JSON.stringify(users.concat(newUser), null, "\t"));
-//};
 
 export const verifyPassword = async (username, password) => {
   const user = await getUser(username);
